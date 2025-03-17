1762711de1069070072b2a6786f2d5fd87486752
TiagoOlivoto <34458429+TiagoOlivoto@users.noreply.github.com>
2025-03-16 23:04:27 -0300

    keep only used layers

--- R/utils_mosaic.R
+++ R/utils_mosaic.R
@@ -2307,8 +2307,8 @@ mosaic_index <- function(mosaic,
       if(in_memory){
         if(index %in% ind$Index){
           formula <- as.character(ind$Equation[as.character(ind$Index)==index])
-        lyrs <- used_layers(indices, formula)
-        names(lyrs) <- names(indices)[lyrs]
+          lyrs <- used_layers(indices, formula)
+          names(lyrs) <- names(indices)[lyrs]
           mosaic_gray <- terra::lapp(mosaic[[lyrs]], parse_formula(formula, lyrs))
         } else{
           mosaic_gray <- terra::lapp(mosaic[[lyrs]], parse_formula(index, lyrs))
